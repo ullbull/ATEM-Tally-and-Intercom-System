bd95defd6f605590bf3e384f2ee0f90a8f375336
ullbull <ulrik.karlsson7@gmail.com>
2020-12-14 09:08:06 +0100

    testfile

--- test/receive_mic.py
+++ test/receive_mic.py
@@ -28,11 +28,6 @@ client_socket, address = s.accept()
 # if below code is executed, that means the sender is connected
 print(f"[+] {address} is connected.")
 
-
-
-
-"""PyAudio Example: Play a wave file (callback version)."""
-
 import pyaudio
 import wave
 import time
@@ -42,19 +37,16 @@ WIDTH = 2
 CHANNELS = 2
 RATE = 44100
 
-# if len(sys.argv) < 2:
-#     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
-#     sys.exit(-1)
-
-# wf = wave.open(sys.argv[1], 'rb')
-
 # instantiate PyAudio (1)
 p = pyaudio.PyAudio()
 
 # define callback (2)
 def callback(in_data, frame_count, time_info, status):
     global chunk_size
+
+    # Receive data that will be added to the stream
     data = client_socket.recv(chunk_size)
+    
     return (data, pyaudio.paContinue)
 
 # open stream using callback (3)
@@ -77,33 +69,6 @@ stream.close()
 
 # close PyAudio (7)
 p.terminate()
-
-
-
-
-
-
-
-
-
-
-# # receive using client socket, not server socket
-# received_bytes = []
-# empty = sys.getsizeof(client_socket.recv(0))
-# size = empty + 1
-# while size > empty:
-#     bytes_read = client_socket.recv(chunk_size)
-#     size = sys.getsizeof(bytes_read)
-#     received_bytes.append(bytes_read)
-
-# # # Print received data
-# print('received: ')
-# i = 0
-# for bytes_read in received_bytes:
-#     print(f'''byte {i}:
-    
-# {bytes_read}''')
-#     i += 1
 
 # close the client socket
 client_socket.close()
