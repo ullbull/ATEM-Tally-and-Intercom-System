6f53bec2519db3ef4e321ed401d9765f09f3d2a0
ullbull <ulrik.karlsson7@gmail.com>
2020-12-21 09:45:17 +0100

    Accessing mic in browser

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,24 @@
 console.log('Welcome!');
 const socket = io();
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+// Check if client has GetUserMedia()
+if (hasGetUserMedia()) {
+
+  const constraints = {
+    audio: true,
+  };
+
+  const audio = document.querySelector("#autoAudio");
+
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    audio.srcObject = stream;
+  });
+
+} else {
+  alert("getUserMedia() is not supported by your browser");
+}
+
