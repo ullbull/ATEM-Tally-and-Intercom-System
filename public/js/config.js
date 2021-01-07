ca36916ae4e6f47a552183d0d0225d1486c26199
ullbull <ulrik.karlsson7@gmail.com>
2021-01-07 04:52:55 +0100

    Added chrome settings link

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,12 @@
 const atemIpInput = document.getElementById('atem-ip-input');
 
+let url = window.location.hostname + ':' + window.location.port
+
+const body = document.getElementsByTagName("BODY")[0]
+const p = document.createElement("p");
+p.innerHTML = `Go to link above and add the following address: ${url}`;
+body.appendChild(p);
+
 fetch('/get-config')
   .then(response => response.json())
   .then(data => {
